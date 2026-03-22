0eb03b7f7ca956ecba88bfb8f297769a5652d345
MindCl0ud <84906227+MindCl0ud@users.noreply.github.com>
2026-03-23 06:56:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          "react-vendor": ["react", "react-dom"],
-          "firebase-vendor": ["firebase"],
-        },
-      },
-    },
-  },
   publicDir: "public",
 });
